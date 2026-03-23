b3ce7ffdeed792d69250c3cc4adc6e8b7041a8d4
Arpit Malik <arpit08malik2000@gmail.com>
2026-03-23 18:05:31 +0530

    added fetch timeline feature

---
+++ src/main/java/dev/arpit/scaler/services/IScheduledLectureService.java
@@ -0,0 +1,13 @@
+package com.example.scaler.services;
+
+import com.example.scaler.exceptions.InvalidLearnerException;
+import com.example.scaler.models.ScheduledLecture;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface LearnerService {
+
+    public List<ScheduledLecture> fetchTimeline(long learnerId) throws InvalidLearnerException;
+}
